3d83ef4b7bd9d715b4b0eb2dd6c2a2846033beea
Allen Phillip Pottinger <allenpottinger@gmail.com>
2020-03-24 17:16:34 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,3 +1,23 @@
-class Anagram 
-  attr_accessor :word 
-  
+class Anagram
+
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array)
+    arr = []
+    array.each do |anagram|
+      if anagram.split("").sort == @word.split("").sort
+        arr << anagram
+      end
+    end
+    arr
+  end
+end
+
+listen = Anagram.new("listen")
+listen.match(%w(enlists google inlets banana))
+
+
